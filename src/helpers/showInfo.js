1b81d8e5ff459382092ba43e9089589254cd49cd
MatthieuMosch <mmosch@gmail.com>
2025-02-05 22:01:40 +0100

    opmerking van terri verwerkt

--- src/helpers/showInfo.js
+++ src/helpers/showInfo.js
@@ -1,11 +1,7 @@
 // show info about tv from input
 
 function showInfo(tv) {
-    const info =
-        tv.brand + " " +
-        tv.type + " - " +
-        tv.name;
-    return info;
+    return tv.brand + " " + tv.type + " - " + tv.name;
 }
 
 export  default showInfo;
